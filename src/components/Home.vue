d6a02487db03e2fd08fe1cec9bcf5fc9791fb16d
wxd <1360273288@qq.com>
2020-09-05 21:26:19 +0800

    登录功能完成

---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <el-button type="info" @click="logout">退出</el-button>
+    </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout () {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+      this.$message({
+        duration: 500,
+        message: '退出成功！',
+        type: 'success'
+      })
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
